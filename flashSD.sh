ef80dddaa1edb5c68f95539a84e66940224f1d00
FilipOwsiany <filowsi29@gmail.com>
2025-09-22 17:22:54 +0200

    chore: Updated script and submodule

--- flashSD.sh
+++ flashSD.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 (
-    lsblk | grep sdb >> /dev/null
+    lsblk | grep sda >> /dev/null
     is_present=$?
     if [ $is_present -ne 0 ]; then
-        echo "No /dev/sdb found. Exiting."
+        echo "No /dev/sda found. Exiting."
         exit 1
     fi
-    echo "Found /dev/sdb."
+    echo "Found /dev/sda."
 
-    cd poky-rpi/build/tmp/deploy/images/raspberrypi5 || exit 1
+    cd poky-rpi/build/tmp/deploy/images/raspberrypi5-gateway || exit 1
     echo "Flashing..."
 
     set -e
 
-    bzcat gateway-image-raspberrypi5.rootfs.wic.bz2 | sudo dd of=/dev/sdb bs=4M status=progress conv=fsync
+    bzcat gateway-image-raspberrypi5-gateway.rootfs.wic.bz2 | sudo dd of=/dev/sda bs=4M status=progress conv=fsync
     echo "Done."
 )
